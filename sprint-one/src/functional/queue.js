75c27809394d1e7c658563abaa75c0b51b36c3b4
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-08-20 10:29:09 -0600

    Update functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -18,7 +18,7 @@ var Queue = function(){
   someInstance.dequeue = function(){
     console.log(storage);
     var result = storage[start];
-    delete storage[start];
+    // delete storage[start];
     if (count - start) {
       start++;
     }
@@ -31,28 +31,3 @@ var Queue = function(){
 
   return someInstance;
 };
-
-var test = Queue();
-console.log(test.enqueue('foo'));
-// foo
-console.log(test.size());
-// 1
-console.log(test.enqueue('bar'));
-// bar
-console.log(test.size());
-// 2
-console.log(test.dequeue());
-// foo
-console.log(test.size());
-// 1
-console.log(test.enqueue('arb'));
-// arb
-console.log(test.size());
-// 2
-console.log(test.dequeue());
-// bar
-console.log(test.size());
-// 1
-
-
-//{val: value, last: }
