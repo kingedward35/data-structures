74f266031c7191a46bb9e94909d18a84f2879bcd
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-08-19 20:56:09 -0600

    Finished functional pattern stacks and queues

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,5 +1,6 @@
 var Queue = function(){
   var count = 0;
+  var start = 0;
   var someInstance = {};
 
   // Use an object with numeric keys to store values
@@ -8,21 +9,50 @@ var Queue = function(){
   // Implement the methods below
 
   someInstance.enqueue = function(value){
+    console.log(storage);
     storage[count] = value;
     count++;
     return value;
   };
 
   someInstance.dequeue = function(){
-    if (count) {
-      count--;
+    console.log(storage);
+    var result = storage[start];
+    delete storage[start];
+    if (count - start) {
+      start++;
     }
-    return storage[count];
+    return result;
   };
 
   someInstance.size = function(){
-    return count;
+    return count - start;
   };
 
   return someInstance;
 };
+
+var test = Queue();
+console.log(test.enqueue('foo'));
+// foo
+console.log(test.size());
+// 1
+console.log(test.enqueue('bar'));
+// bar
+console.log(test.size());
+// 2
+console.log(test.dequeue());
+// foo
+console.log(test.size());
+// 1
+console.log(test.enqueue('arb'));
+// arb
+console.log(test.size());
+// 2
+console.log(test.dequeue());
+// bar
+console.log(test.size());
+// 1
+
+
+//{val: value, last: }
