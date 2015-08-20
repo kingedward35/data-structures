d0a40fb7c1953f76ec90ae3f285eec3f3c882f60
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-08-20 17:16:16 -0600

    Functional shared stack complete

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,42 @@
+var _ = {};
+
+_.extend = function(obj) {
+  each(slice.call(arguments, 1), function(source) {
+    if (source) {
+      for (var prop in source) {
+        obj[prop] = source[prop];
+      }
+    }
+  });
+  return obj;
+};
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  someInstance.count = 0;
+  someInstance.storage = {};
+  _.extend(someInstance, stackMethods);
+  return someInstance;
 };
 
 var stackMethods = {};
+
+stackMethods.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+};
+
+stackMethods.pop = function () {
+  if (this.count) {
+    this.count--;
+  }
+  var result = this.storage[this.count];
+  delete this.storage[this.count];
+  return result;
+};
+
+stackMethods.size = function() {
+  return this.count;
+};
 
 
