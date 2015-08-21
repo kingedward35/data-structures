0876faea17ed0536a534da3b2f863ae17b57e086
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-08-21 12:51:45 -0600

    Finished Linklist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,36 @@
 var LinkedList = function(){
+  console.log("New test starting");
   var list = {};
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value){
+    var newTail = Node(value);
+    if (list.tail) {
+     list.tail.next = newTail;
+    } else {
+      list.head = newTail;
+    }
+    list.tail = newTail;
   };
 
   list.removeHead = function(){
+    var result = list.head.value;
+    var next = list.head.next;
+    delete list.head;
+    list.head = next;
+    return result;
   };
 
   list.contains = function(target){
+    var currentNode = list.head;
+    while (currentNode !== null) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+    return false;
   };
 
   return list;
@@ -27,3 +48,9 @@ var Node = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// LinkedList: constant
+// addToTail: constant
+// removeHead: constant
+// contains: linear
+// Node: constant
