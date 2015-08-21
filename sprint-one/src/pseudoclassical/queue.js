8c2bd910fdeddbf6abb4ea12e6856d75753f0516
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-08-20 18:04:12 -0600

    Finished pseudoclassical queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.count = 0;
+  this.start = 0;
 };
 
+Queue.prototype.enqueue = function(value) {
+	this.storage[this.count] = value;
+	this.count++;
+};
 
+Queue.prototype.dequeue = function() {
+	var result = this.storage[this.start];
+	delete this.storage[this.start];
+	if (this.count - this.start) {
+		this.start++;
+	}
+
+	return result;
+};
+
+Queue.prototype.size = function() {
+	return this.count - this.start;
+};
