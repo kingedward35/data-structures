f682512aa490bc4d9db52da688733c9c4fa1fa1d
Verlon Smith <verlonsmith@Verlons-MacBook-Air.local>
2015-08-22 16:51:27 -0600

    Finished Binary Search Tree and HashTable

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,50 @@
 var BinarySearchTree = function(value){
+  var tree = {};
+  tree.value = value;
+  _.extend(tree, binarySearchTreeMethods);
+  return tree;
+};
+
+binarySearchTreeMethods = {};
+
+binarySearchTreeMethods.insert = function(value) {
+  if (value < this.value) {
+    this.left ? this.left.insert(value) : this.left = BinarySearchTree(value);
+  }
+  else if (value > this.value) {
+    this.right ? this.right.insert(value) : this.right = BinarySearchTree(value);
+  }
+};
+
+binarySearchTreeMethods.contains = function(value) {
+  if (this.value === value) {
+    return true;
+  }
+  if (value < this.value && this.left) {
+    if (this.left.contains(value)) {
+      return true;
+    }
+  }
+  if (value > this.value && this.right) {
+    if (this.right.contains(value)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+binarySearchTreeMethods.depthFirstLog = function(fn) {
+  fn(this.value);
+  this.left && this.left.depthFirstLog(fn);
+  this.right && this.right.depthFirstLog(fn);
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+// BinarySearchTree: constant
+// insert: logarithmic
+// contains: logarithmic
+// depthFirstLog: linear
--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,39 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  if (!bucket) {
+    this._storage.set(i, [[k, v]]);
+  } else {
+    var pair = _.find(bucket, function(pair) {
+      return pair[0] === k;
+    });
+    if (pair) {
+      pair[1] = v;
+    } else {
+      bucket.push([k, v]);
+    }
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  var bucket = this._storage.get(i);
+  if (bucket) {
+    var pair = _.find(bucket, function(pair) {
+      return pair[0] === k;
+    });
+    return pair[1];
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  var bucket = this._storage.get(i);
+  var pair = _.find(bucket, function(pair) {
+    return pair[0] === k;
+  });
+  pair[1] = null;
 };
 
 
